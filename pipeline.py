0db4bf1de51da078d9962639b33c038745bce8fe
vector-client-token <>
2020-12-05 00:24:20 +0000

    Update workbook

--- pipeline.py
+++ pipeline.py
@@ -1,2 +1,9 @@
 
 
+@transform_pandas(
+    Output(rid="ri.vector.main.execute.d672107c-8b84-46b8-afe7-40f1629460fd"),
+    inpatient_scaled_w_imputation=Input(rid="ri.foundry.main.dataset.f410db35-59e0-4b82-8fa8-d6dc6a61c9f2")
+)
+def unnamed(inpatient_scaled_w_imputation):
+    
+
