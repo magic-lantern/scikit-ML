482b34df977efd7cdcb32e06db803ffa482c7a0a
vector-client-token <>
2020-12-09 22:06:00 +0000

    Update workbook

--- pipeline.py
+++ pipeline.py
@@ -204,6 +204,7 @@ def lr_rfecv(data_and_outcomes, inpatient_scaled_w_imputation, outcomes):
     lr_rfecv=Input(rid="ri.foundry.main.dataset.32b0e775-ba50-44e2-ae82-5f41ec31a84c")
 )
 def pca_rfecv_cols(data_and_outcomes, lr_rfecv):
+    print(lr_rfecv.columns)
     arr = data_and_outcomes.select(lr_rfecv.columns).toPandas().values
     pca_all = PCA(random_state=42)
     pca_all.fit(arr)
