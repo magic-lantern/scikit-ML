d64b9e368d5ee2b0537758abcec0d721b51f1d1b
vector-client-token <>
2020-12-10 21:19:02 +0000

    Update workbook

--- pipeline.py
+++ pipeline.py
@@ -817,6 +817,7 @@ def svm_gs(data_scaled_and_outcomes, outcomes, inpatient_scaled_w_imputation):
         #'C': param_range
     }
 
+    # run time with default env and cache_size 1600 - 376
     svm = SVC(kernel='linear', random_state=my_random_state, probability=True, cache_size=1600)
     #gd = GridSearchCV(estimator=svm, param_grid=parameters, cv=5)
     #gd.fit(x_train, y_train)
