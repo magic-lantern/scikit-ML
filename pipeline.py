7a7b7db54e9819c93e75c637c076e521dffe13bd
vector-client-token <>
2020-12-10 22:36:53 +0000

    Update workbook

--- pipeline.py
+++ pipeline.py
@@ -525,12 +525,12 @@ def rf_best_feat( outcomes, data_encoded_and_outcomes, inpatient_encoded_w_imput
 
     y_pred = rf.predict(x_test)
     confmat = confusion_matrix(y_true=y_test, y_pred=y_pred)
-    print('rf w 500 estimators w/gini')
+    print('rf w 750 estimators w/gini')
     print(confmat)
     y_pred = rf.predict_proba(x_test)[:, 1]
     print('ROC_AUC_SCORE: ', roc_auc_score(y_true=y_test, y_score=y_pred))
 
-    fig, (ax1, ax2) = plt.subplots(2, figsize=(10,10))
+    fig, (ax1, ax2) = plt.subplots(2, figsize=(12,10))
     fig.tight_layout(h_pad=4)
 
     rf_disp = plot_roc_curve(rf, x_test, y_test, ax=ax1)
@@ -544,7 +544,7 @@ def rf_best_feat( outcomes, data_encoded_and_outcomes, inpatient_encoded_w_imput
     #ax2.set_xticklabels(my_data.columns[indices])
     #plt.xticks(range(x_train.shape[1]), my_data.columns[indices], rotation=90)
     ax2.set_xticks(range(x_train.shape[1]))
-    ax2.set_xticklabels(list(my_data.columns[indices]), rotation=-45, ha='left', fontsize=6)
+    ax2.set_xticklabels(list(my_data.columns[indices]), rotation=-45, ha='left', fontsize=10)
     ax2.set_xlim([-1, x_train.shape[1]])
 
     plt.subplots_adjust(bottom=0.2)
