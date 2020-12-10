3ee4cccb27ed9be8f7acc6ed44328e0e3823d133
vector-client-token <>
2020-12-10 22:02:06 +0000

    Update workbook

--- pipeline.py
+++ pipeline.py
@@ -894,8 +894,8 @@ def svm_gs(data_scaled_and_outcomes, outcomes, inpatient_scaled_w_imputation):
     # run time with high-mem env and cache_size 1600 - 382  sec
     # run time with default env and cache_size 800 -  sec
     # run time with high-mem env and cache_size 800 - 407 sec
-    # run time with high-mem env and cache_size 2400 - 407 sec  
-    svm = SVC(kernel='linear', random_state=my_random_state, probability=True, cache_size=2400)
+    # run time with high-mem env and cache_size 2400 - 446 sec  
+    svm = SVC(kernel='linear', gamma=10, random_state=my_random_state, probability=True, cache_size=1600)
     #gd = GridSearchCV(estimator=svm, param_grid=parameters, cv=5)
     #gd.fit(x_train, y_train)
     #print(gd.best_params_)
