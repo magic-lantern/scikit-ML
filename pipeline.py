9f56f1a600a076d34470ea259568548901194614
vector-client-token <>
2020-12-11 22:10:39 +0000

    Update workbook

--- pipeline.py
+++ pipeline.py
@@ -674,13 +674,13 @@ def rf_best_hyp_params( outcomes, data_encoded_and_outcomes, inpatient_encoded_w
     outcomes=Input(rid="ri.foundry.main.dataset.3d9b1654-3923-484f-8db5-6b38b56e290c")
 )
 def rf_gs( outcomes, data_encoded_and_outcomes, inpatient_encoded_w_imputation):
-start = timeit.default_timer()
-data_and_outcomes = data_encoded_and_outcomes
-my_data = data_and_outcomes.select(inpatient_encoded_w_imputation.columns).toPandas()
-my_data = my_data.drop(columns='visit_occurrence_id')
-my_outcomes = data_and_outcomes.select(outcomes.columns).toPandas()
-y = my_outcomes.bad_outcome
-x_train, x_test, y_train, y_test = train_test_split(my_data, y, test_size=0.3, random_state=1, stratify=y)
+    start = timeit.default_timer()
+    data_and_outcomes = data_encoded_and_outcomes
+    my_data = data_and_outcomes.select(inpatient_encoded_w_imputation.columns).toPandas()
+    my_data = my_data.drop(columns='visit_occurrence_id')
+    my_outcomes = data_and_outcomes.select(outcomes.columns).toPandas()
+    y = my_outcomes.bad_outcome
+    x_train, x_test, y_train, y_test = train_test_split(my_data, y, test_size=0.3, random_state=1, stratify=y)
 
     parameters = {
         'n_estimators':[250,500,750],
