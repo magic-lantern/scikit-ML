ee2b38f56157cb0f1c06163e3e0694e3fb155eea
vector-client-token <>
2020-12-12 00:31:19 +0000

    Update workbook

--- pipeline.py
+++ pipeline.py
@@ -889,7 +889,7 @@ def svm_gs_1(data_scaled_and_outcomes, outcomes, inpatient_scaled_w_imputation):
     parameters = {
         'kernel':['rbf'],
         'gamma': ['scale', 'auto'],
-        'C': 1.0
+        'C': [1.0]
         #'C': [0.0001, 0.001, 0.01, 0.1, 1.0, 10.0, 100.0, 1000.0]
     }
 
