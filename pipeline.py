05a42bffc3340bdf1e0e97e95636083c4b794b79
vector-client-token <>
2020-12-13 04:34:00 +0000

    Update workbook

--- pipeline.py
+++ pipeline.py
@@ -345,9 +345,11 @@ def lr_gs(data_scaled_and_outcomes, inpatient_scaled_w_imputation, outcomes):
     x_train, x_test, y_train, y_test = train_test_split(my_data, y, test_size=0.3, random_state=1, stratify=y)
 
     parameters = {
-        'penalty': ['l2'],#['none', 'l1', 'l2', 'elasticnet'],
-        'solver': ['newton-cg', 'lbfgs', 'liblinear', 'sag', 'saga'],
-        'C': [0.01, 0.1, 0.25, 0.5, 0.75, 1.0, 2.0, 10.0]
+        'penalty': ['elasticnet'],#['none', 'l1', 'l2', 'elasticnet'],
+        #'solver': ['newton-cg', 'lbfgs', 'liblinear', 'sag', 'saga'],
+        'solver': ['saga']
+        'l1_ratio': [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
+        #'C': [0.01, 0.1, 0.25, 0.5, 0.75, 1.0, 2.0, 10.0]
     }
 
     lr = LogisticRegression(random_state=my_random_state,
