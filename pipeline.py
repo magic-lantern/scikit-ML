2c5512eb41cc30b0ba4a656e075746f5777aa613
vector-client-token <>
2020-12-13 05:13:43 +0000

    Update workbook

--- pipeline.py
+++ pipeline.py
@@ -1047,3 +1047,13 @@ def svm_sigmoid_gs(data_scaled_and_outcomes, outcomes, inpatient_scaled_w_imputa
     stop = timeit.default_timer()
     print('Time: ', stop - start)  
 
+@transform_pandas(
+    Output(rid="ri.vector.main.execute.504b52f9-b065-484f-b14a-4f5786d59db7"),
+    inpatient_scaled_w_imputation=Input(rid="ri.foundry.main.dataset.f410db35-59e0-4b82-8fa8-d6dc6a61c9f2"),
+    outcomes=Input(rid="ri.foundry.main.dataset.3d9b1654-3923-484f-8db5-6b38b56e290c")
+)
+def data_scaled_and_outcomes_1(inpatient_scaled_w_imputation, outcomes):
+    i = inpatient_scaled_w_imputation
+    o = outcomes
+    return i.join(o, on=['visit_occurrence_id'], how='inner')
+
