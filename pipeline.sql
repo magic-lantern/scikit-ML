1a9da638c96714df523e24a7fe28ad84598df754
vector-client-token <>
2020-12-13 05:06:49 +0000

    Update workbook

--- pipeline.sql
+++ pipeline.sql
@@ -1,9 +1,14 @@
 
 
 @transform_pandas(
-    Output(rid="ri.vector.main.execute.ddeb99b5-9f4a-4582-8037-9d4c0c0f43a6"),
+    Output(rid="ri.foundry.main.dataset.45a8018b-2074-42a4-b3a3-fb60ed35ac0f"),
     data_encoded_and_outcomes=Input(rid="ri.foundry.main.dataset.32069249-a675-4faf-9d3c-a68ff0670c07")
 )
-SELECT *
+SELECT year_end, month_end, bad_outcome, count(1) as rec_count
+FROM (
+SELECT year(visit_end_date) as year_end, month(visit_end_date) as month_end, bad_outcome
 FROM data_encoded_and_outcomes
+)
+GROUP BY year_end, month_end, bad_outcome
+order by year_end, month_end, bad_outcome
 
