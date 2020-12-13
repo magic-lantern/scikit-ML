a7722bc00e0bffec0186603939b00ad018431d52
vector-client-token <>
2020-12-13 05:04:48 +0000

    Update workbook

--- pipeline.sql
+++ pipeline.sql
@@ -1,2 +1,9 @@
 
 
+@transform_pandas(
+    Output(rid="ri.vector.main.execute.ddeb99b5-9f4a-4582-8037-9d4c0c0f43a6"),
+    data_encoded_and_outcomes=Input(rid="ri.foundry.main.dataset.32069249-a675-4faf-9d3c-a68ff0670c07")
+)
+SELECT *
+FROM data_encoded_and_outcomes
+
